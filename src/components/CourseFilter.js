c08c30129c04e8a3bcb847a49fd2fd10664b39bd
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2022-07-27 13:40:09 -0400

    removed Account; wip for future dev

--- src/components/CourseFilter.js
+++ src/components/CourseFilter.js
@@ -276,22 +276,7 @@ export default class CourseFilter extends React.Component {
                 </div>
               </li>
     
-              <li className="border-top my-3"></li>
-    
-              <li className="mb-1">
-                <button className="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                  Account
-                </button>
-    
-                <div className="collapse" id="account-collapse">
-                  <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">New...</a></li>
-                    <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Profile</a></li>
-                    <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Settings</a></li>
-                    <li><a href="#" className="link-dark d-inline-flex text-decoration-none rounded">Sign out</a></li>
-                  </ul>
-                </div>
-              </li>
+              
     
             </ul>
           </div>

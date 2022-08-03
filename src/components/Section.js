56946111221fa633b830e4ad8527b0eb7b3ad944
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2022-08-03 00:44:30 -0400

    updated scrollbar

--- src/components/Section.js
+++ src/components/Section.js
@@ -19,18 +19,20 @@ function Section() {
         <div className = {(sectionOverlayVisible ? "overlayFadeIn" : "overlayFadeOut") + " overlay"} id = "section-overlay">
             <div className = "course-sections p-4 bg-white rounded">
 
-                <div className = "d-flex border-bottom">
-                    <div id = "sections-text">
-                        <h5 className = "custom-fs-5">{ selectedCourseCode } Sections</h5>
-                        <p className = "custom-small text-muted">Showing {selectedSections.length} Results</p>
+                <div id = "course-sections-overflow" className = "p-4">
+                    <div className = "d-flex border-bottom">
+                        <div id = "sections-text">
+                            <h5 className = "custom-fs-5">{ selectedCourseCode } Sections</h5>
+                            <p className = "custom-small text-muted">Showing {selectedSections.length} Results</p>
+                        </div>
+                        <i className = "bi bi-x ms-auto" style = {{fontSize: "2em"}} id="section-close-btn"
+                        onClick={ closeSectionBtnHandler }></i>
                     </div>
-                    <i className = "bi bi-x ms-auto" style = {{fontSize: "2em"}} id="section-close-btn"
-                    onClick={ closeSectionBtnHandler }></i>
-                </div>
 
-                {selectedSections.map(section => (
-                    <SectionItem key={section.registrationNumber} {...section} />
-                ))}
+                    {selectedSections.map(section => (
+                        <SectionItem key={section.registrationNumber} {...section} />
+                    ))}
+                </div>
 
             </div>
         </div>
